46759bba675be10f311243d71a37910ed288467f
ILYA BODUNOV <airfox@mail.ru>
2024-09-16 01:50:00 +0300

    16.09.2024 #3

---
+++ 1.py
@@ -0,0 +1,23 @@
+with open("1.txt","a+") as f:
+    for x in range(100):
+        # f.write("0"*x+'\n')
+        # f.write(f"{'0'*x}\n")
+
+# with open("1.txt","r") as f:
+#     for x in f:
+#         print(x.strip())
+
+
+
+# with open("1.txt","r") as f:
+#     m=f.readline()
+#     print (m)
+#     m = f.readline()
+#     print(m)
+
+
+# with open("1.txt","r") as f:
+#     m=f.readlines()
+#     print (m)
+#
+##
